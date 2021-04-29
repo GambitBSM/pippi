758ddf658819dc2a8cc20cc1d2603de149dabd0e
Tomas Gonzalo <gonzalo@physik.rwth-aachen.de>
2021-04-29 10:21:21 +0200

    another one

--- pippi_read.py
+++ pippi_read.py
@@ -361,7 +361,7 @@ def getChainData(filename, cut_all_invalid=None, requested_cols=None, assignment
         # Read in any python preamble specified in the pip file.
         if preamble is not None: exec(preamble)
         exec('data[lookup_key['+str(i)+']] = '+expression)
-      except KeyError, e:
+      except KeyError as e:
         print('ERROR: Datastream '+str(e)+', which you have tried to define a function of')
         print('in assign_to_pippi_datastream, is not itself defined as a datastream.')
         print('This usually happens because you have not requested it for plotting in')
